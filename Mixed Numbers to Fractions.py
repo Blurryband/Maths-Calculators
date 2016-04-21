895e8a1d14aa69a738dbc91a63c19e0eefa34840
Blurryband <samuel_morris@icloud.com>
2016-04-21 20:37:25 +1000

    Update Mixed Numbers to Fractions.py

--- Mixed Numbers to Fractions.py
+++ Mixed Numbers to Fractions.py
@@ -1,6 +1,7 @@
-Whole = input("Enter in Whole Number")
-Num = input("Enter in the Numerator ")
-Dem = input("Enter in the Denominator ")
-A1 = Whole * Dem
-A2 = A1 + Num
-print A2
+while true:
+  Whole = input("Enter in Whole Number")
+  Num = input("Enter in the Numerator ")
+  Dem = input("Enter in the Denominator ")
+  A1 = Whole * Dem
+  A2 = A1 + Num
+  print A2
